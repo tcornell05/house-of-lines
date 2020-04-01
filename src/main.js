22cb50fd270d0b22772aa7ca68d2200faac4c7e1
root <root@DESKTOP-VVCKFBD.localdomain>
2020-03-31 23:40:52 -0700

    removing old code

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false;
 
 new Vue({
   data: {
-    consoleData: ""
   },
   render: h => h(App),
 }).$mount('#app');
